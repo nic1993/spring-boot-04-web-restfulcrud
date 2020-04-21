6a0c946b3f26c0edc309ecd316ea594d782752e4
caiyunke <caiyunke@corp.netease.com>
2020-04-21 15:29:16 +0800

    test

--- src/main/java/com/atguigu/springboot04webrestfulcrud/SpringBoot04WebRestfulcrudApplication.java
+++ src/main/java/com/atguigu/springboot04webrestfulcrud/SpringBoot04WebRestfulcrudApplication.java
@@ -10,5 +10,6 @@ public class  SpringBoot04WebRestfulcrudApplication {
 
      public static void main(String[] args) {
         SpringApplication.run(SpringBoot04WebRestfulcrudApplication.class, args);
+        String s;
     }
 }
